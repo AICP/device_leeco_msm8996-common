f70f3e0c4d7ca635e29f12809b9ddca10c923ebd
Marko Man <darkobas@gmail.com>
2017-05-09 17:22:07 +0200

    add qcom bootjars
    Change-Id: I4724e4b4b440206bc7cb84c39bdf9f5e6ef24f63

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,6 +184,8 @@ USE_SENSOR_MULTI_HAL := true
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
 
+PRODUCT_BOOT_JARS += com.qti.dpmframework dpmapi com.qti.location.sdk
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
