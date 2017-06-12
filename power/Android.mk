845cf6fe4ee77d119688d88b9edd3d2f5bd76b43
mosimchah <mosimchah@gmail.com>
2017-06-12 11:33:33 -0400

    Revert "Import Power HAL from SultanXDA"
    This reverts commit 0885a93f412cb0a3c9ffb6b3524140d14652b1bb.
    
    Change-Id: Ie17af01f5de461ce74fa594eccc5f54a447c67ff

--- power/Android.mk
+++ power/Android.mk
@@ -1,30 +1,26 @@
-#
-# Copyright 2014 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+LOCAL_PATH := $(call my-dir)
 
-LOCAL_PATH:= $(call my-dir)
-
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
+
+# Include target-specific files.
+ifeq ($(call is-board-platform-in-list, msm8996), true)
+LOCAL_SRC_FILES += power-8996.c
 endif
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := power.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_MODULE_TAGS := optional
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+#ifneq ($(TARGET_USES_AOSP),true)
+#    LOCAL_CFLAGS += -DEXTRA_POWERHAL_HINTS
+#endif
+
 LOCAL_MODULE := power.msm8996
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
