765897d1abb5d9ba3805e3ad911eaecbebfae651
Harsh Shandilya <msfjarvis@gmail.com>
2017-08-13 15:32:10 +0530

    msm8996-common: Add bootanimation binary optimisations
    Change-Id: Idfe35f311067124da10c1e1a2a2fe78222a9f0b8

--- msm8996.mk
+++ msm8996.mk
@@ -61,9 +61,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
-# Ramdisk
-PRODUCT_PACKAGES += \
-    init.power.sh
+# Boot animation
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_PRELOAD := true
 
 # Audio
 PRODUCT_COPY_FILES += \
