b8df294552a9afcefbc344a01063e74cb44b1158
maxwen <max.weninger@gmail.com>
2017-10-09 03:09:53 -0400

    Add thermal hidl service
    Change-Id: I30abd53cfae1101157b92bf9b869e6469915c9f5

--- msm8996.mk
+++ msm8996.mk
@@ -309,8 +309,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    thermal.msm8996 \
     android.hardware.thermal@1.0-impl \
-    thermal.msm8996
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
