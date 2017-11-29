06b85362f750083728d18cd9f8f10d13cc656b97
TheScarastic <warabhishek@gmail.com>
2017-11-29 15:35:47 +0530

    Add custom biometrics to fox goodix fpsensor
     * Both fpc and goodix are supported
     * Do not use some notifying cancel hacks like https://github.com/AOSPA/android_frameworks_base/commit/acbb85687fc577ff74d0a1de93ccd63c2b05985a
       it is already implimented in Biometricservice and may cause issues.
     * SuperSu before SR5-superSU-v.2.82 seems to have an issue so please use latest SuperSu.
    
    co-authored with @alexstsv2
    
    Change-Id: Ib2450c45827ae21c63017d8b69a8016481f30b98
    Signed-off-by: TheScarastic <warabhishek@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service \
+    android.hardware.biometrics.fingerprint@2.0-service-custom \
     fingerprintd \
     fingerprint.msm8996
 
