3348b925742d29158f89001e18dd6655e0276bee
mosimchah <mosimchah@gmail.com>
2017-12-13 01:45:37 -0500

    switch back to the caf power hal
    Change-Id: I68622d09689aa0b159d98573aac51e04790c8b05

--- msm8996.mk
+++ msm8996.mk
@@ -269,7 +269,11 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    power.msm8996
+    android.hardware.power@1.0-service \
+    power.qcom
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
