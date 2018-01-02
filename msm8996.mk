23b31d75faff79e8755327b5ec019e4f2cc8f959
mosimchah <mosimchah@gmail.com>
2018-01-02 15:32:14 -0500

    use camera2 until Snap is ready
    Change-Id: I905619e1fc76a3ad9041ba00026de6c92e402e0c

--- msm8996.mk
+++ msm8996.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     libshims_camera \
     libshims_qcamera-daemon \
-    Snap
+    Camera2
 #    vendor.qti.hardware.camera.device@1.0 \
 
 # Connectivity Engine support (CNE)
