19e4b5ea562dfb616041a3b20c14cdda061f59d5
mosimchah <mosimchah@gmail.com>
2018-01-05 07:53:24 -0500

    Revert "msm8996: remove libjni_livedisplay"
    This reverts commit 8d6cd56dfbdd11f20016dad150cd5054991b3544.
    
    Change-Id: I3007e9d55e085633f4567d2f8b0e44e6fcb384cc

--- msm8996.mk
+++ msm8996.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0-java
 
+# Display calibration
+PRODUCT_PACKAGES += \
+    libjni_livedisplay
+
 # Doze mode
 PRODUCT_PACKAGES += \
     Doze
@@ -236,8 +240,8 @@ PRODUCT_PACKAGES += \
     lights.msm8996
 
 # LiveDisplay native
-#PRODUCT_PACKAGES += \
-#    libjni_livedisplay
+PRODUCT_PACKAGES += \
+    libjni_livedisplay
 
 # Media
 PRODUCT_COPY_FILES += \
