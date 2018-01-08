5c241bbbf91a413cace13e7ebeba0fcd890397a0
mosimchah <mosimchah@gmail.com>
2018-01-08 04:46:35 -0500

    remove duplicate android.hardware.thermal@1.0-impl
    Change-Id: Id33e229539f35e0113829b03eeedd8dc40cdcf81

--- msm8996.mk
+++ msm8996.mk
@@ -310,7 +310,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
     thermal.msm8996 \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
