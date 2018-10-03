d4fed00200ef3ec54b1ca0f5a5cb4fff62cdad80
Michele Bono <bono.michele94@gmail.com>
2018-10-03 17:54:40 +0200

    AICP-ify doze
    Change-Id: I5d87390b70ca8d83941313bd204529e9ec3411d0

--- doze/src/com/custom/ambient/display/Utils.java
+++ doze/src/com/custom/ambient/display/Utils.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package com.custom.ambient.display;
 
 import android.content.Context;
 import android.content.Intent;
