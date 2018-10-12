ce6c0a84c1c58a6972dfde96d4f94b9a7fcb7d6a
andr68rus <ashtmb@gmail.com>
2018-10-12 14:25:26 +0300

    msm8996-common: add LeParts
    Change-Id: I98143214e5f6306a5474624be82176cdc157dc23

--- msm8996.mk
+++ msm8996.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LeParts settigs modules
+PRODUCT_PACKAGES += \
+    LeParts
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
