0b15281e4993c8d53c09e96d6b0d4df0a2dde754
mosimchah <mosimchah@gmail.com>
2018-10-14 10:24:44 -0400

    msm8996: AICP-ify
    Change-Id: I2790d5efc1507fcfe4ef357a7e5e04e44d571699

--- msm8996.mk
+++ msm8996.mk
@@ -20,8 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
@@ -140,7 +139,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.device@1.0-impl \
     camera.msm8996 \
-    Snap
+    SnapdragonCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
@@ -251,10 +250,6 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
-
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
