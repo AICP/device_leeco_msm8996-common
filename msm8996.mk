53b6bd3bac8f39d1895dac3ecafefa48e15b96c9
mosimchah <mosimchah@gmail.com>
2019-05-14 21:51:37 -0400

    msm8996: Add live display
    -Based on pex op3
    Change-Id: Ieabeac280dbffff3a51280d23496ff8bcccc5e0b

--- msm8996.mk
+++ msm8996.mk
@@ -251,6 +251,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+   libjni_livedisplay
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
