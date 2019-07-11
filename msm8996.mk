92cc5b2bd0947c151fcf09436cf2c72e8f7bb39f
Moshe <mosimchah@gmail.com>
2019-07-11 22:33:19 +0200

    msm8996: Use Snap cam from aex
    Change-Id: I48408d3d34aab7dd6aac5f4f6ebfbed5d79c10a4

--- msm8996.mk
+++ msm8996.mk
@@ -127,7 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
-    SnapdragonCamera
+    Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
