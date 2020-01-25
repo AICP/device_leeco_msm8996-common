e46070df793b350425bb9017bf6daa4e54a47f91
Bruno Martins <bgcngm@gmail.com>
2020-01-25 21:00:00 +0000

    Revert "msm8996-common: Make shim for libui"
    No longer required with Q blobs.
    
    This reverts commit e044e48418ef56a2f116713761e246f260ae42e4.
    
    Change-Id: If4a97452391d195be19c6e5026731164278896b4
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -211,11 +211,6 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/lib/libui.so|libui_shim.so \
-    /system/lib64/libui.so|libui_shim.so
-
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
