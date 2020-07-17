cc8b0042c1ec454539ead332726756b380376b14
mosimchah <mosimchah@gmail.com>
2020-07-17 11:52:09 -0400

    msm8996-coomon: AICP-ify
    Change-Id: Id05b8f946636a05c78c05e2cde90fea4dc5c3de1

--- msm8996.mk
+++ msm8996.mk
@@ -420,8 +420,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
