3927172899cbbd962a1b28bc9f71a4fbb552c268
Marc Bourgoin <themard85@gmail.com>
2023-02-14 21:12:27 -0700

    [DNM]msm8996-common: Nuke Lineage addons
    Change-Id: I19058bbefc1921ea76a78f5e8c4c723b36565d4b

--- msm8996.mk
+++ msm8996.mk
@@ -282,14 +282,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.leeco_8996
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -460,10 +452,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
-# Trust HAL
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
